39c313c8529caed812bc3f1e67ecfabbcafbbf4d
6u6a <15762254497@139.com>
2017-11-21 21:46:45 +0800

    change char register function.

--- dram.c
+++ dram.c
@@ -3,15 +3,15 @@
 //
 //	This module implements a Linux character-mode device-driver
 //	for the processor's installed physical memory.  It utilizes
-//	the kernel's 'kmap()' function, as a uniform way to provide  
+//	the kernel's 'kmap()' function, as a uniform way to provide
 //	access to all the memory-zones (including the "high memory"
-//	on systems with more than 896MB of installed physical ram). 
-//	The access here is 'read-only' because we deem it too risky 
+//	on systems with more than 896MB of installed physical ram).
+//	The access here is 'read-only' because we deem it too risky
 //	to the stable functioning of our system to allow every user
 //	the unrestricted ability to arbitrarily modify memory-areas
 //	which might contain some "critical" kernel data-structures.
-//	We implement an 'llseek()' method so that users can readily 
-//	find out how much physical processor-memory is installed. 
+//	We implement an 'llseek()' method so that users can readily
+//	find out how much physical processor-memory is installed.
 //
 //	NOTE: Developed and tested with Linux kernel version 2.6.10
 //
@@ -21,37 +21,58 @@
 //	revised on: 06 FEB 2008 -- for machines having 4GB of memory
 //-------------------------------------------------------------------
 
-#include <linux/module.h>	// for module_init() 
+#include <linux/module.h>	// for module_init()
 #include <linux/highmem.h>	// for kmap(), kunmap()
-#include <asm/uaccess.h>	// for copy_to_user() 
+#include <asm/uaccess.h>	// for copy_to_user()
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
 
 char modname[] = "dram";	// for displaying driver's name
-int my_major = 85;		// note static major assignment 
+int my_major = 85;		// note static major assignment
 unsigned long	dram_size;		// total bytes of system memory
 
 loff_t my_llseek( struct file *file, loff_t offset, int whence );
 ssize_t my_read( struct file *file, char *buf, size_t count, loff_t *pos );
 
-struct file_operations 
+struct file_operations
 my_fops =	{
 		owner:		THIS_MODULE,
 		llseek:		my_llseek,
 		read:		my_read,
 		};
 
+static struct miscdevice dram_dev =
+{
+    /*
+     * 内核动态生成minor
+     */
+    MISC_DYNAMIC_MINOR,
+    /*
+     * 设备名字见宏DEV_NAME
+     */
+    modname,
+    /*
+     * 绑定文件操作函数
+     */
+    &my_fops
+};
+
 static int __init dram_init( void )
 {
 	printk( "<1>\nInstalling \'%s\' module ", modname );
 	printk( "(major=%d)\n", my_major );
-	
+
 	dram_size = (unsigned long)num_physpages << PAGE_SHIFT;
 	printk( "<1>  ramtop=%08llX (%llu MB)\n", dram_size, dram_size >> 20 );
-	return 	register_chrdev( my_major, modname, &my_fops );
+	printk("PAGE_OFFSET=%08llx\n", PAGE_OFFSET);
+	return 	misc_register(&dram_dev);
+	//return 	register_chrdev( my_major, modname, &my_fops );
 }
 
 static void __exit dram_exit( void )
 {
-	unregister_chrdev( my_major, modname );
+	misc_deregister(&dram_dev);
+	//unregister_chrdev( my_major, modname );
 	printk( "<1>Removing \'%s\' module\n", modname );
 }
 
@@ -60,15 +81,15 @@ ssize_t my_read( struct file *file, char *buf, size_t count, loff_t *pos )
 	struct page	*pp;
 	void		*from;
 	int		page_number, page_indent, more;
-	
+
 	// we cannot read beyond the end-of-file
 	if ( *pos >= dram_size ) return 0;
 
 	// determine which physical page to temporarily map
-	// and how far into that page to begin reading from 
+	// and how far into that page to begin reading from
 	page_number = *pos / PAGE_SIZE;
 	page_indent = *pos % PAGE_SIZE;
-	
+
 	// map the designated physical page into kernel space
 	/*If kerel vesion is 2.6.32 or later, please use pfn_to_page() to get page, and include
 	    asm-generic/memory_model.h*/
@@ -77,21 +98,21 @@ ssize_t my_read( struct file *file, char *buf, size_t count, loff_t *pos )
 #else
 	pp = &mem_map[ page_number ];
 #endif
-	
+
 	from = kmap( pp ) + page_indent;
-	
+
 	// cannot reliably read beyond the end of this mapped page
 	if ( page_indent + count > PAGE_SIZE ) count = PAGE_SIZE - page_indent;
 
-	// now transfer count bytes from mapped page to user-supplied buffer 	
+	// now transfer count bytes from mapped page to user-supplied buffer
 	more = copy_to_user( buf, from, count );
-	
+
 	// ok now to discard the temporary page mapping
 	kunmap( pp );
-	
+
 	// an error occurred if less than count bytes got copied
 	if ( more ) return -EFAULT;
-	
+
 	// otherwise advance file-pointer and report number of bytes read
 	*pos += count;
 	return	count;
